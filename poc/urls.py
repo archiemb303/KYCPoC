04982eb4d9505158f7c2e259eae02ced2e2117ed
Gaurav Rajput <105104456+Gauravraj1141@users.noreply.github.com>
2024-08-18 21:17:49 +0530

    update new changes

--- poc/urls.py
+++ poc/urls.py
@@ -39,9 +39,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('los-api/', include('los_app.urls')),
  
-   re_path(r'swagger/$', schema_view.with_ui('swagger',
+   path('', schema_view.with_ui('swagger',
         cache_timeout=0), name='schema-swagger-ui'),
 
-    re_path(r'^redoc/$', schema_view.with_ui('redoc',
+    path('redoc/', schema_view.with_ui('redoc',
             cache_timeout=0), name='schema-redoc'),
 ]
